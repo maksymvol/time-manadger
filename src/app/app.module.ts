97642590005e859592ed4bb0b57672a833b960a0
Maksym Volkov <maksymvo@tahometer.com>
2019-03-22 15:04:44 +0200

    Project card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {TasksListComponent} from './tasks-list/tasks-list.component';
@@ -10,17 +14,15 @@ import {HttpClientModule} from '@angular/common/http';
 import {ServerService} from './server.service';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {TasksPreviewListComponent} from './tasks-preview-list/tasks-preview-list.component';
-
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { ProjectCardComponent } from './project-card/project-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TasksListComponent,
     ProjectsListComponent,
-    TasksPreviewListComponent
+    TasksPreviewListComponent,
+    ProjectCardComponent
   ],
   imports: [
     BrowserModule,
